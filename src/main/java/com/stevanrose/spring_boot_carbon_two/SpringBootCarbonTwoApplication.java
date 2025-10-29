c7425ea426265b556391faccd3f1316be18f1efd
Stevan Rose <stevan.rose@gmail.com>
2025-10-29 15:58:16 +0000

    Initial commit
    Contains basic generated spring-boot project with:
    
    - Web
    - JPA
    - Lombok
    - Postgres

---
+++ src/main/java/com/stevanrose/spring_boot_carbon_two/SpringBootCarbonTwoApplication.java
@@ -0,0 +1,13 @@
+package com.stevanrose.spring_boot_carbon_two;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCarbonTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCarbonTwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stevanrose/spring_boot_carbon_two/SpringBootCarbonTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stevanrose.spring_boot_carbon_two;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCarbonTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
